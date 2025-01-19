d6c3674b774c9e59615c2c4da3af6c9da116b0a4
norberttech <norbert@orzechowicz.pl>
2025-01-19 14:19:45 +0000

    Added array related functions (#1391)

---
+++ src/Flow/ETL/Function/ArrayFilter.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Flow\ETL\Function;
+
+use Flow\ETL\Row;
+
+final class ArrayFilter extends ScalarFunctionChain
+{
+    public function __construct(private readonly ScalarFunction|array $array, private readonly mixed $value = null)
+    {
+    }
+
+    public function eval(Row $row) : mixed
+    {
+        $array = (new Parameter($this->array))->asArray($row);
+
+        if (null === $array) {
+            return null;
+        }
+
+        $value = (new Parameter($this->value))->eval($row);
+
+        return \array_filter($array, fn ($item) => $item !== $value);
+    }
+}
